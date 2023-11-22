ab494e4a00c94a2c57d7e260d63477680fd5d4c8
JIBINJJOHNY <jibinjjohny11@gmail.com>
2023-11-22 13:05:41 +0100

    Added Procfile

--- cakecravings/settings.py
+++ cakecravings/settings.py
@@ -26,8 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-mm1a9)41v)3-wi(7p*l%er*-9ma!nr(_f(k0bui5h5a89#@w2v'
-
+SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
